a886fbbd737de5a0f5bd9395a1ff0b8375b7ee4f
Ibrahim V K <89912630+ibrhmvk@users.noreply.github.com>
2022-10-08 17:33:52 +0530

    2nd one

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './App.css'
-import {InboxIcon, HomeIcon, ProfileIcon, SearchIcon ,BarIcon, AddIcon } from "./icons/TabBar"
-import VideoJS from './VideoJs'
+import { InboxIcon, HomeIcon, ProfileIcon, SearchIcon, BarIcon, AddIcon } from "./icons/TabBar"
+// import VideoJS from './VideoJs'
+import ReactPlayer from 'react-player'
 
 
 const API_URL =
@@ -10,56 +11,88 @@ const API_URL =
 export default function App() {
   const [videos, setVideos] = useState([])
   const playerRef = React.useRef(null);
-  const [isVisible, setIsVisible] = useState(0)
+  const [loading, setloading] = useState(true)
+  const [isVideoPlaying, setisVideoPlaying] = useState(false);
+  const [currentPlayer, setCurrentPlayer] = useState('');
+
+  const vidRef = useRef();
+
+  useEffect(() => {
+    const scroll = document.getElementById("video");
+
+    if (scroll) {
+      scroll.addEventListener("scroll", (e) => {
+        console.log('scrolling')
+        setCurrentPlayer('asdas')
+      });
+    }
+  }, []);
 
   useEffect(() => {
     fetch(API_URL)
       .then((data) => data.json())
-      .then((data) => setVideos(data))
+      .then((data) => {
+        setVideos(data)
+        setCurrentPlayer(data[0].video)
+        setloading(false)
+      })
   }, [])
 
-  const handlePlayerReady = (player) => {
-    playerRef.current = player;
 
-    // You can handle player events here, for example:
-    player.on('waiting', () => {
-      videojs.log('player is waiting');
-    });
-
-    player.on('dispose', () => {
-      videojs.log('player will dispose');
-    });
+  const onVideoClick = (id) => {
+    setCurrentPlayer(id);
   };
+
+  console.log(currentPlayer)
+
 
   return (
     <div className="app ">
-       
-      <div className="video-container snap-mandatory snap-y scrollbar-hide shadow-2xl relative" >
-        {videos.map((item, index) => <VideoJS options={{
-          autoplay: isVisible === index,
-          controls: true,
-          loop : true,
-          paused: isVisible != index,
-          responsive: true,
-          fluid: true,
-          userActions: {
-            doubleClick: false
-          },
-          sources: [{
-            src: item.video,
-            type: 'application/x-mpegURL'
-          }]
-        }} onReady={handlePlayerReady} />
+      {/* 
+      {loading ?
+        <div className="w-full bg-slate-200">
+
+        </div> : */}
+      <div className="video-container  snap-mandatory snap-y scrollbar-hide shadow-2xl relative" id="video" >
+        {videos.map((item, index) =>
+          <div className='relative'>
+            <button onClick={() => onVideoClick(item.video)} key={index} >
+              <ReactPlayer
+                ref={vidRef}
+                className="snap-always snap-start "
+                playing={currentPlayer === item.video}
+                width='100%'
+                loop
+                height='100%'
+                url={item.video} />
+              <p className='absolute z-50 top-[85%] left-4 text-white text-base font-semibold'>@{item.name}</p>
+            </button>
+          </div>
         )}
-        <div className='bg-black flex p-5 justify-between w-[42.770vh] fixed z-50 top-[85%]'>
-        <HomeIcon/>
-        <SearchIcon/>
-        <AddIcon/>
-        <InboxIcon/>
-        <ProfileIcon/>
-       </div>
-      </div> 
-      
+        <div className='bg-black flex p-5 justify-between w-[42.760vh] fixed z-50 top-[85%] items-center'>
+          <div className='w-[20px] flex flex-col justify-center items-center'>
+            <HomeIcon />
+            <p className='text-white text-[8px]'>Home</p>
+          </div>
+          <div className='flex flex-col justify-center items-center'>
+            <div className='w-[20px]'>
+              <SearchIcon />
+            </div>
+            <p className='text-white text-[8px]'>Discover</p>
+          </div>
+          <div className='w-[40px]'>
+            <AddIcon />
+          </div>
+          <div className='w-[20px] flex flex-col justify-centeritems-center'>
+            <InboxIcon />
+            <p className='text-white text-[8px]'>Inbox</p>
+          </div>
+          <div className='w-[20px] flex flex-col justify-center items-center'>
+            <ProfileIcon />
+            <p className='text-white text-[8px]'>Me</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/VideoJs.jsx
+++ src/VideoJs.jsx
@@ -1,53 +1,57 @@
-import React from 'react';
-import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
+// import React, { useState } from 'react';
+// import videojs from 'video.js';
+// import 'video.js/dist/video-js.css';
 
 
-export const VideoJS = (props) => {
-  const videoRef = React.useRef(null);
-  const playerRef = React.useRef(null);
-  const { options, onReady } = props;
+// export const VideoJS = (props) => {
+//   const [isVideoPlaying, setisVideoPlaying] = useState(false);
 
-  React.useEffect(() => {
+//   const videoRef = React.useRef(null);
+//   const playerRef = React.useRef(null);
+//   const { options, onReady } = props;
 
-    // Make sure Video.js player is only initialized once
-    if (!playerRef.current) {
-      const videoElement = videoRef.current;
+//   React.useEffect(() => {
+//     console.log('second ref running')
+//     // Make sure Video.js player is only initialized once
+//     if (!playerRef.current) {
+//       const videoElement = videoRef.current;
 
-      if (!videoElement) return;
+//       if (!videoElement) return;
 
-      const player = playerRef.current = videojs(videoElement, options, () => {
-        videojs.log('player is ready');
-        onReady && onReady(player);
-      });
+//       const player = playerRef.current = videojs(videoElement, options, () => {
+//         videojs.log('player is ready');
+//         onReady && onReady(player);
+//       });
 
-      // You could update an existing player in the `else` block here
-      // on prop change, for example:
-    } else {
-      // const player = playerRef.current;
+//       // You could update an existing player in the `else` block here
+//       // on prop change, for example:
+//     } else {
+//       // const player = playerRef.current;
 
-      // player.autoplay(options.autoplay);
-      // player.src(options.sources);
-    }
-  }, [options, videoRef]);
+//       // player.autoplay(options.autoplay);
+//       // player.src(options.sources);
+//     }
+//   }, [options, videoRef]);
 
-  // Dispose the Video.js player when the functional component unmounts
-  React.useEffect(() => {
-    const player = playerRef.current;
 
-    return () => {
-      if (player) {
-        player.dispose();
-        playerRef.current = null;
-      }
-    };
-  }, [playerRef]);
 
-  return (
-    <div data-vjs-player className='snap-mandatory snap-y '>
-      <video ref={videoRef} className='video-js vjs-big-play-centered snap-always snap-start  bg-[#dce0b76e]' />
-    </div>
-  );
-}
+//   // Dispose the Video.js player when the functional component unmounts
+//   React.useEffect(() => {
+//     const player = playerRef.current;
 
-export default VideoJS;
+//     return () => {
+//       if (player) {
+//         player.dispose();
+//         playerRef.current = null;
+//       }
+//     };
+//   }, [playerRef]);
+
+//   return (
+//     <div data-vjs-player className='snap-mandatory snap-y ' >
+//       <video ref={videoRef} className='video-js vjs-big-play-centered snap-always snap-start  bg-[#dce0b76e]' />
+//     </div>
+//   );
+// }
+
+// export default VideoJS;
