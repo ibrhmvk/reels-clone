dc22aaf183ee60e406621e55a598472f5b70e6ba
Ibrahim V K <89912630+ibrhmvk@users.noreply.github.com>
2022-10-09 14:23:56 +0530

    scrolling working

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
-import { InboxIcon, HomeIcon, ProfileIcon, SearchIcon, BarIcon, AddIcon } from "./icons/TabBar"
-// import VideoJS from './VideoJs'
-import ReactPlayer from 'react-player'
-
+import { InboxIcon, HomeIcon, ProfileIcon, SearchIcon, AddIcon } from "./icons/TabBar"
+import Video from './Video'
 
 const API_URL =
   'https://63149e7ffc9dc45cb4f082c1.mockapi.io/videos'
 
 export default function App() {
   const [videos, setVideos] = useState([])
-  const playerRef = React.useRef(null);
   const [loading, setLoading] = useState(true)
-  const [currentPlayer, setCurrentPlayer] = useState('');
   const [error, setError] = useState(null);
-
-  const vidRef = useRef();
-
-  useEffect(() => {
-    const scroll = document.getElementById("video");
-
-    if (scroll) {
-      scroll.addEventListener("scroll", (e) => {
-        console.log('scrolling')
-        setCurrentPlayer('asdas')
-      });
-    }
-  }, []);
 
   useEffect(() => {
     fetch(API_URL)
@@ -40,7 +23,6 @@ export default function App() {
       })
       .then((actualData) => {
         setVideos(actualData);
-        setCurrentPlayer(actualData[0].video)
         setError(null);
       })
       .catch((err) => {
@@ -52,11 +34,7 @@ export default function App() {
       });
   }, []);
 
-  const onVideoClick = (id) => {
-    setCurrentPlayer(id);
-  };
 
-  console.log(currentPlayer)
   return (
     <div className="app">
       {loading && <div role="status">
@@ -69,20 +47,10 @@ export default function App() {
       {error && (
         <div>{`There is a problem fetching the post data - ${error}`}</div>
       )}
-      <div className="video-container  snap-mandatory snap-y scrollbar-hide shadow-2xl relative" id="video" >
-        {videos && videos.map((item, index) =>
-          <button onClick={() => onVideoClick(item.video)} key={index} className='relative' >
-            <ReactPlayer
-              ref={vidRef}
-              className="snap-always snap-start "
-              playing={currentPlayer === item.video}
-              width='43.6vh'
-              loop
-              height='77.7vh'
-              url={item.video} />
-            <p className='absolute z-50 top-[85%] left-4 text-white text-base font-semibold'>@{item.name}</p>
-          </button>
-        )}
+      <div className="video-container  snap-mandatory snap-y scrollbar-hide shadow-2xl relative">
+        {videos && videos.map(({ video, name }) =>
+          <Video key={video} url={video} name={name} />)
+        }
         <div className='bg-black flex p-5 justify-between w-[43.6vh] fixed z-50 top-[87%] items-center'>
           <div className='w-[20px] flex flex-col justify-center items-center'>
             <HomeIcon />
@@ -112,51 +80,3 @@ export default function App() {
 }
 
 
-//   return (
-//     <div className="app ">
-//       {/* 
-//       {loading ?
-//         <div className="w-full bg-slate-200">
-
-//         </div> : */}
-//       <div className="video-container  snap-mandatory snap-y scrollbar-hide shadow-2xl relative" id="video" >
-//         {videos.map((item, index) =>
-//             <button onClick={() => onVideoClick(item.video)} key={index} className='relative' >
-//               <ReactPlayer
-//                 ref={vidRef}
-//                 className="snap-always snap-start "
-//                 playing={currentPlayer === item.video}
-//                 width='43.6vh'
-//                 loop
-//                 height='77.7vh'
-//                 url={item.video} />
-//               <p className='absolute z-50 top-[85%] left-4 text-white text-base font-semibold'>@{item.name}</p>
-//             </button>
-//         )}
-//         <div className='bg-black flex p-5 justify-between w-[43.6vh] fixed z-50 top-[87%] items-center'>
-//           <div className='w-[20px] flex flex-col justify-center items-center'>
-//             <HomeIcon />
-//             <p className='text-white text-[8px]'>Home</p>
-//           </div>
-//           <div className='flex flex-col justify-center items-center'>
-//             <div className='w-[20px]'>
-//               <SearchIcon />
-//             </div>
-//             <p className='text-white text-[8px]'>Discover</p>
-//           </div>
-//           <div className='w-[40px]'>
-//             <AddIcon />
-//           </div>
-//           <div className='w-[20px] flex flex-col justify-centeritems-center'>
-//             <InboxIcon />
-//             <p className='text-white text-[8px]'>Inbox</p>
-//           </div>
-//           <div className='w-[20px] flex flex-col justify-center items-center'>
-//             <ProfileIcon />
-//             <p className='text-white text-[8px]'>Me</p>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
---
+++ src/Video.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./Video.css";
+import useElementOnScreen from './hooks/useElementOnScreen'
+import ReactHlsPlayer from 'react-hls-player'
+
+const Video = ({ url, name }) => {
+  const [playing, setPlaying] = useState(false);
+  const playerRef = React.useRef();
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3
+  }
+  const isVisibile = useElementOnScreen(options, playerRef)
+  const onVideoClick = () => {
+    if (playing) {
+      playerRef.current.pause();
+      setPlaying(!playing);
+    } else {
+      playerRef.current.play();
+      setPlaying(!playing);
+    }
+  };
+  useEffect(() => {
+    if (isVisibile) {
+      if (!playing) {
+        playerRef.current.play();
+        setPlaying(true)
+      }
+    }
+    else {
+      if (playing) {
+        playerRef.current.pause();
+        setPlaying(false)
+      }
+    }
+  }, [isVisibile])
+
+
+  return (
+    <div className="video">
+      {/* <video className="video_player snap-always snap-start" loop preload="true" ref={videoRef} onClick={onVideoClick}>
+        <source src={url} />
+      </video> */}
+
+      <ReactHlsPlayer 
+      className="video_player snap-always snap-start"
+      loop preload="true" playerRef={playerRef} onClick={onVideoClick}
+      src={url}
+
+      />
+      <p className='absolute z-50 top-[85%] left-4 text-white text-base font-semibold'>@{name}</p>
+    </div>
+  );
+};
+export default Video;
+
+
+
